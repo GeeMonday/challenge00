adbbff868a3c7124e2b059ff1b41a03e3f049cdc
GeeMonday <gmonday@academic.rrc.ca>
2024-05-09 18:33:42 -0500

    added sample code

--- hello.rb
+++ hello.rb
@@ -1,1 +1,8 @@
 puts "hello world!"
+ghost_quota = 37
+if (caught_ghosts > ghost_quota)
+  puts "You are done for the day."
+else
+  ghosts_required = ghost_quota - caught_ghosts
+  puts "You need to find #{ghosts_required} more ghosts."
+end 

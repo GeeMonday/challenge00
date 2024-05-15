cd0168effde57ef176f3d40c7ed829d0afe05086
GeeMonday <gmonday@academic.rrc.ca>
2024-05-15 08:18:53 -0500

    modified code

--- challenge00.rb
+++ challenge00.rb
@@ -4,31 +4,33 @@ WEBD_3011-S2-2024SP
 challenge00
 =end
 
-#dollar amount in a varaiable called sub_total
-# Input
-print "Enter the subtotal: $"
-sub_total = gets.chomp.to_f    #This is used to read the user input and remove the trailing newline character
-                                #from input string while it converts the input into a floating point number
-#sub_total = 5.00
-# GST  rate
-GST = 0.04
-# PST rate
-PST= 0.02
-# Grand total
-grand_total = sub_total * GST
-pst_amount = sub_total * PST
-grand_total = sub_total + GST + PST  
-puts "sub total: $#{sub_total}"
-puts "PST: $#{GST} - #{(GST * 100).to_i}%"
-puts "GST: $#{PST} - #{(PST * 100).to_i}%" # added a methodd to convert to integer
-puts "Grand Total: $#{grand_total}"
-
-# Display message based on grand total
-if grand_total <= 5.00
-    puts "Pocket Change"
+  print "Enter the subtotal: "
+  sub_total = gets.chomp.to_f #This is used to read the user input and remove the trailing newline character
+                              #from input string while it converts the input into a floating point numb
+  
+  # Constants for tax rates
+  GST_RATE = 0.07
+  PST_RATE = 0.05
+  
+  # Calculating tax amounts
+  gst_amount = sub_total * GST_RATE
+  pst_amount = sub_total * PST_RATE
+  
+  # Calculating grand total
+  grand_total = sub_total + gst_amount + pst_amount  
+  
+  # Output
+  puts "Subtotal: $#{'%.2f' % sub_total}"
+  puts "PST: $#{'%.2f' % pst_amount} - #{(PST_RATE * 100).to_i}%"
+  puts "GST: $#{'%.2f' % gst_amount} - #{(GST_RATE * 100).to_i}%"
+  puts "Grand Total: $#{'%.2f' % grand_total}"
+  
+  # Display message based on grand total
+  if grand_total <= 5.00
+      puts "Pocket Change"
   elsif grand_total > 5.00 && grand_total < 20
-    puts "Wallet Time"
+      puts "Wallet Time"
   else
-    puts "Charge It!"
+      puts "Charge It!"
   end
-
+  
